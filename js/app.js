e72963575e7d6d9f63277c2dd119330722c4d0a5
utente <utente@NBCHIWYL0012>
2017-02-07 12:27:34 +0100

    terzo commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,2 @@
 var pressione = angular.module('pressione',[] );
 
-pressione.controller('pressioneController', function($scope,$http){ 
-        var vm = this;
-            vm.init=function (){
-                $http.get("../ng-pa/elenco.php")
-                        .then(function(response){
-                            $scope.table =response.data;
-                    });
-                };
-                vm.init();
-        });
---
+++ js/pressioneController.js
@@ -0,0 +1,10 @@
+pressione.controller('pressioneController', function($scope,$http){ 
+        var vm = this;
+            vm.init=function (){
+                $http.get("../ng-pa/elenco.php")
+                        .then(function(response){
+                            $scope.table =response.data;
+                    });
+                };
+                vm.init();
+        });
