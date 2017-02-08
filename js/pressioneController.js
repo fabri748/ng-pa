93069658119a72568e17a04cc74a6c46c0316771
utente <utente@NBCHIWYL0012>
2017-02-08 09:44:23 +0100

    sesto commit-aggiunto delete

--- js/pressioneController.js
+++ js/pressioneController.js
@@ -1,18 +1,18 @@
-pressione.controller('pressioneController',["$scope", "$http", function ($scope, $http) {
-    var vm = this;
-    vm.init = function () {
-        $http.get("../ng-pa/elenco.php")
-                .then(function (response) {
-                    $scope.table = response.data;
-                });
-    };
-    vm.delete = function (id) {
-        $http.get("../ng-pa/elenco.php?act=del&id="+id)
-                .then(function (response) {
-                    $scope.table = response.data;
-        
-    });
-    };
-    vm.init();
-    
-}]);
+pressione.controller('pressioneController', ["$scope", "$http", function ($scope, $http) {
+        var vm = this;
+        vm.init = function () {
+            $http.get("../ng-pa/elenco.php")
+                    .then(function (response) {
+                        $scope.table = response.data;
+                    });
+        };
+        vm.delete = function (id) {
+            $http.get("../ng-pa/elenco.php?act=del&id=" + id)
+                    .then(function (response) {
+                        $scope.table = response.data;
+
+                    });
+        };
+        vm.init();
+
+    }]);
